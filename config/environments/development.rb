12d8a7fd83a8629e77c81a45493d9dadf7702906
Zachary Spector <zjspector@gmail.com>
2014-08-10 17:53:05 -0400

    Fix syntax error in config file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,12 +27,7 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = true
 
-  # Adds additional error checking when serving assets at runtime.
-  # Checks for improperly declared sprockets dependencies.
-  # Raises helpful error messages.
-  config.assets.raise_runtime_errors = true
-
-  config.action_mailer.smtp_setting = {
+  config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
     domain: Rails.application.secrets.domain_name,
@@ -44,11 +39,17 @@ Rails.application.configure do
 
   # ActionMailer Config
   config.action_mailer.default_url_options = {
-    :host => 'localhost:3000'
+    :host => '10.10.10.10:3000'
   }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
-  }
+
+  # Adds additional error checking when serving assets at runtime.
+  # Checks for improperly declared sprockets dependencies.
+  # Raises helpful error messages.
+  config.assets.raise_runtime_errors = true
+
+
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
