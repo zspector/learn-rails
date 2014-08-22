0783507111f686a540d760f325b3a9ab3b8c41c7
Zachary Spector <zjspector@gmail.com>
2014-08-22 18:09:12 -0400

    Update application layout

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,6 +2,8 @@ class VisitorsController < ApplicationController
 
   def new
     @owner = Owner.new
+    flash.now[:notice] = 'Welcome!'
+    flash.now[:alert] = 'My Birthday is soon!'
   end
 
 end
